00d55081b512bc08f6958e17b8ac299c68af2ab2
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-07-02 22:39:40 +0530

    connect database

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const env = require('dotenv');
+const express = require("express");
+const env = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 
 env.config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: "hello world"})
-})
+console.log(process.env.DB_USER,process.env.DB_PASSWORD,process.env.DB_NAME)
 
-console.log("server runnig at port 8000")
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yj5lf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+).then(()=>{
+    console.log('Database connected')
+}).catch(error => {
+    console.log(error)
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hello world" });
+});
+
+console.log(`server running at port ${PORT}`);
 app.listen(PORT);
