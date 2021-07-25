97c5d08f2f91946e442cb493899af0e386aa4543
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-07-25 15:09:36 +0530

    Signin page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import Form from "../components/Form";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+}));
 
 const SignUp = () => {
-    return (
-        <div>
-            SignUp
-        </div>
-    )
-}
+  const classes = useStyles();
 
-export default SignUp
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+  return (
+    <div className={classes.root}>
+      <div>
+        <Form
+          title={"SignUp"}
+          button={{
+            label: "SignUp",
+            handleClick: () => {},
+          }}
+          inputs={
+            [
+              {
+                label: "Email Id",
+                type: "text",
+                handleChange: () => {},
+              },
+              {
+                label: "Password",
+                type: "password",
+                handleChange: () => {},
+              },
+            ]
+          }
+          handleSubmit={handleSubmit}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
