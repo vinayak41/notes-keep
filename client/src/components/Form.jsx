a854a254d2caad83c16529af6b5f5d087f0fae2f
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-07-25 15:03:33 +0530

    Form component

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import { Button, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: "10px",
+    padding: "1rem 1rem 2rem",
+    '& label.Mui-focused': {
+      color: 'black',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'grey',
+      },
+      '&:hover fieldset': {
+        borderColor: 'grey',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'grey',
+      },
+    },
+  },
+  inputsWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "1rem",
+  },
+  inputField: {
+    width: "18rem",
+    margin: "0.5rem"
+  }
+}));
+
+const Form = ({title, button, inputs, handleSubmit}) => {
+  const classes = useStyles();
+  return (
+    <Paper component="form" onSubmit={handleSubmit} elevation={5} className={classes.root}>
+      <h1>{title}</h1>
+      <div className={classes.inputsWrapper}>
+        {inputs.map(({ label, type, handleChange }) => (
+          <TextField
+            className={classes.inputField}
+            key={label}
+            type={type}
+            label={label}
+            onChange={handleChange}
+            variant="outlined"
+          />
+        ))}
+      </div>
+      <Button variant="contained" type="submit">{button.label}</Button>
+    </Paper>
+  );
+};
+
+export default Form;
