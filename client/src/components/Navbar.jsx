7048e30b9438bdfc92e8ccb6cedbbf2a5199696b
vinayak Khandekar <khandekarvinayak41@gmail.com>
2022-01-15 18:59:15 +0530

    feat: logout user

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { IconButton, makeStyles } from "@material-ui/core";
+import { IconButton, makeStyles, Button } from "@material-ui/core";
 import appLogo from "../assets/logo.png";
-import Brightness4Icon from '@material-ui/icons/Brightness4';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+// import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import LoadingLine from "./LoadingLine";
-import {changeTheme} from "../redux/actions/themeActions"
+import { changeTheme } from "../redux/actions/themeActions";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,22 +21,28 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     fontWeight: theme.typography.fontWeightBold,
-    flexGrow: "1",
+    flexGrow: "1"
   },
   appLogo: {
-    width: "2rem",
-  },
+    width: "2rem"
+  }
 }));
 
-export default function Navbar() {
+const Navbar = (props) => {
   const classes = useStyles();
-  const dispatch = useDispatch()
-  const themeType = useSelector(state => state.theme.type)
+  const dispatch = useDispatch();
+  const themeType = useSelector((state) => state.theme.type);
 
+  const { isAuthenticated } = useSelector((state) => state.user);
 
   const handleThemeChange = () => {
-    dispatch(changeTheme(themeType === "light" ? "dark" : "light"))
-  }
+    dispatch(changeTheme(themeType === "light" ? "dark" : "light"));
+  };
+
+  const handelLogout = () => {
+    sessionStorage.setItem("token", "");
+    window.location.href = "/signin";
+  };
 
   return (
     <AppBar className={classes.root}>
@@ -45,10 +51,19 @@ export default function Navbar() {
         <Typography variant="h4" className={classes.title}>
           Note Keep
         </Typography>
-        <IconButton onClick={handleThemeChange}><Brightness4Icon /></IconButton>
-        <IconButton><AccountCircleIcon /></IconButton>
+        <IconButton onClick={handleThemeChange}>
+          <Brightness4Icon />
+        </IconButton>
+        {/* <IconButton><AccountCircleIcon /></IconButton> */}
+        {isAuthenticated ? (
+          <Button variant="outlined" onClick={handelLogout}>
+            Logout
+          </Button>
+        ) : null}
       </ToolBar>
       <LoadingLine />
     </AppBar>
   );
 }
+
+export default Navbar
